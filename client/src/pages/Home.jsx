4feffb21f3b2a33233bcaca81287f32ae2701c3a
umangUS002 <btech10271.23@bitmesra.ac.in>
2025-07-27 16:07:09 +0530

    Changes bg to black

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,16 +5,13 @@ import EventsScroller from '../components/Events'
 import Gallery from '../components/Gallery'
 import Blogs from '../components/Blogs'
 import { motion } from 'framer-motion'
-import { assets } from '../assets/assets'
 
 function Home() {
   return (
     <div>
             <Hero />
-<div
-  className="relative text-white overflow-x-hidden bg-fixed bg-cover bg-center"
-  style={{ backgroundImage: `url(${assets.bg})` }}
->
+<div className="relative bg-[#0c0c0c] text-white overflow-x-hidden">
+
       {/* 🔹 Background Blobs - Stationary and behind content */}
       <motion.div
         animate={{ y: [0, -30, 0], x: [0, 20, 0] }}
@@ -24,7 +21,7 @@ function Home() {
           duration: 18,
           ease: "easeInOut",
         }}
-        className="fixed hidden top-3/5 left-[-150px] w-[400px] h-[400px] bg-cyan-500 opacity-30 blur-3xl rounded-full z-10 pointer-events-none"
+        className="fixed top-1/2 left-[-150px] w-[400px] h-[400px] bg-cyan-500 opacity-30 blur-3xl rounded-full z-10 pointer-events-none"
       />
 
       <motion.div
@@ -35,7 +32,7 @@ function Home() {
           duration: 16,
           ease: "easeInOut",
         }}
-        className="fixed hidden bottom-3/5 right-[-150px] w-[400px] h-[400px] bg-cyan-500 opacity-30 blur-3xl rounded-full z-0 pointer-events-none"
+        className="fixed bottom-1/4 right-[-150px] w-[400px] h-[400px] bg-cyan-500 opacity-30 blur-3xl rounded-full z-0 pointer-events-none"
       />
 
       {/* 🔸 Scrollable Content */}
