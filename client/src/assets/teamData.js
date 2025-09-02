58335cd40bd225aac5714cf271d7bb3dfad00fdc
umangUS002 <btech10271.23@bitmesra.ac.in>
2025-09-03 01:42:16 +0530

    updated akshat bhaiya info

--- client/src/assets/teamData.js
+++ client/src/assets/teamData.js
@@ -60,6 +60,14 @@ const seniorExecutives = [
     "instagram": "https://www.instagram.com/dishaan_d_33?igsh=MXU5emo0Y3RjMnhscA==",
     "linkedin": "https://www.linkedin.com/in/dishaan-d-43890a2a2?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
   },
+    {
+    "name": "Akshat Suman",
+    "position": "Director",
+    "image": getImage("35.jpg"),
+    "email": "hi@akshatsuman.com",
+    "instagram": "https://instagram.com/@theakshatsuman",
+    "linkedin": "https://linkedin.com/in/akshatsuman"
+  },
   {
     "name": "Aakanksha Pathak",
     "position": "Director",
