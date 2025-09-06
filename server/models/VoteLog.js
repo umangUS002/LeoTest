ffe4f5cfb9ce353fed3db4288455a31d75232cc8
umangUS002 <btech10271.23@bitmesra.ac.in>
2025-09-06 14:37:11 +0530

    fixed1

--- server/models/VoteLog.js
+++ server/models/VoteLog.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const voteLogSchema = new mongoose.Schema({
   ip: { type: String, required: true, unique: true },
-  contestantId: { type: mongoose.Schema.Types.ObjectId, ref: "Contestant" }
+  contestantId: { type: mongoose.Schema.Types.ObjectId, ref: "Contestants" }
 }, { timestamps: true });
 
 export default mongoose.model("VoteLog", voteLogSchema);
