3a3a4078d61f5b4719333966375f52e6574d58ad
umangUS002 <btech10271.23@bitmesra.ac.in>
2025-10-09 13:34:11 +0530

    added recruitment form

---
+++ server/models/Recruitment.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const recruitmentSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    roll: {type: String, required: true},
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    branch: { type: String, required: true },
+    batch: { type: String, required: true },
+    gender: { type: String, required: true },
+    subTeam: {type: String, required: true},
+    achievements: {type: String},
+    samples: {type: String},
+    ques1: {type: String, required: true},
+    ques2: {type: String, required: true},
+    ques3: {type: String, required: true},
+    ques4: {type: String, required: true},
+})
+
+const Recruitment = mongoose.model("Recruitment", recruitmentSchema);
+
+export default Recruitment;
--- server/routes/regRoutes.js
+++ server/routes/regRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerEvent, getRegistrations } from "../controllers/registrationController.js";
+import { registerEvent, getRegistrations, recruitmentRegistration } from "../controllers/registrationController.js";
 import upload from "../middleware/multer.js";
 
 const regRouter = express.Router();
 
 regRouter.post("/register", upload.single("image"), registerEvent);       // user submits registration
 regRouter.get("/count", getRegistrations);         // admin view all registrations
+regRouter.post("/registerRecruit", upload.single("image"), recruitmentRegistration);
 
 export default regRouter;
